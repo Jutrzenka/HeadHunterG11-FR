bd5d42e1c9e54c58adbb20cf34fdef1c994b18f8
Jutrzenka <kacperczaja1999@gmail.com>
2022-07-22 01:19:32 +0200

    Update eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './_App.scss';
+import moment from 'moment';
+import { Audio } from 'react-loader-spinner';
 
 export const App = () => {
   return (
     <div className="App">
-      <h1>Head Hunter MegaK!</h1>
+      <h1>Test</h1>
+      <h2>Moment.js</h2>
+      <h3>{moment().format()}</h3>
+      <h2>Spinner</h2>
+      <Audio height="100" width="100" color="grey" ariaLabel="loading" />
     </div>
   );
 };
