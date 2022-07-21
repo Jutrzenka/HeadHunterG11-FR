cbe218d8ba3811a3fefb08ade58ad976582d77e5
Jutrzenka <kacperczaja1999@gmail.com>
2022-07-21 23:48:15 +0200

    Configuration _global.scss _mixins.scss _variable.scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import "./styles/_variable.scss";
+import "./styles/_global.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
