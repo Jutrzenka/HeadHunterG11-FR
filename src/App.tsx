578eff3a4ff8a9fcca6a18a297603f89f71d6f90
Jutrzenka <kacperczaja1999@gmail.com>
2022-07-22 01:03:04 +0200

    Configuration eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import "./_App.scss";
+import './_App.scss';
 
 export const App = () => {
   return (
     <div className="App">
-
+      <h1>Head Hunter MegaK!</h1>
     </div>
   );
-}
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
-import "./styles/_variable.scss";
-import "./styles/_global.scss";
+import './styles/_variable.scss';
+import './styles/_global.scss';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
