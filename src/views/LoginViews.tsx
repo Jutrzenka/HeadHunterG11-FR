2db2983df145cf095aadc446deeb08283a263549
Jutrzenka <kacperczaja1999@gmail.com>
2022-07-22 22:29:55 +0200

    Create forms

--- src/views/LoginViews.tsx
+++ src/views/LoginViews.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Button } from '../components/common/Button/Button';
-import { useToggle } from '../utils/useToggle';
+import { Form } from '../components/common/Form/Form';
+import { Input } from '../components/common/Input/Input';
 
 export const LoginViews = () => {
-  const [click, toggleClick] = useToggle();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const sendForm = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(email);
+    console.log(password);
+  };
 
   return (
     <div className="App">
-      <Button title={'Zarejestruj'} toggle={toggleClick} />
+      <Form sendForm={sendForm}>
+        <Input nameValue={'E-mail'} value={setEmail} />
+        <Input nameValue={'Hasło'} value={setPassword} />
+        <Button title={'Zaloguj się'} />
+      </Form>
     </div>
   );
 };
