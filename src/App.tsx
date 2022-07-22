91acec83630600f7b993448b906aec17ac86b8fc
Kacper Czaja <kacperczaja1999@gmail.com>
2022-07-23 01:38:51 +0200

    Added logo and refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { TestViews } from './views/TestViews';
 import { LoginViews } from './views/LoginViews';
 
 import './_App.scss';
@@ -8,8 +7,7 @@ import './_App.scss';
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<TestViews />} />
-      <Route path="/login" element={<LoginViews />} />
+      <Route path="/" element={<LoginViews />} />
     </Routes>
   );
 };
