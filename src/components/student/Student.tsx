de3b3101b955a0816abc77742a0c43a526e95268
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-07-27 15:51:49 +0200

    Added Student Component

---
+++ src/components/student/Student.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { apiUrl } from '../../config/api';
+import { useParams } from 'react-router-dom';
+
+export const Student = () => {
+  const [form, setForm] = useState({ name: '' });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch(`${apiUrl}/student/${id}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': `${apiUrl}`,
+        },
+        referrerPolicy: 'no-referrer',
+        credentials: 'include',
+        mode: 'cors',
+      });
+      const data = await res.json();
+
+      setForm({ name: data.name });
+    })();
+  }, []);
+
+  return (
+    <>
+      <h2>Student</h2>
+      <p>Dane studenta: {form.name}</p>
+      <p>ID studenta: {id}</p>
+    </>
+  );
+};
