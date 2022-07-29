f9eda363bd701a16be9e3ed4c15170b2a79ed63d
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-07-29 20:20:55 +0200

    style AdminNav

--- src/components/admin/AdminNav/AdminNav.tsx
+++ src/components/admin/AdminNav/AdminNav.tsx
@@ -5,9 +5,15 @@ import './_AdminNav.scss';
 export const AdminNav = () => {
   return (
     <div className="component-AdminNav">
+      <div className="logo-wrap">
+        <img src="/img/logo_MegaK.png" alt="logo" />
+        <h1>Panel Administratora</h1>
+      </div>
+
       <nav className="nav">
+        <NavLink to="/list">Lista kursantow </NavLink>
         <NavLink to="/add">Dodaj liste kursantow</NavLink>
-        <NavLink to="/list">Lista kursantow admina</NavLink>
+        <NavLink to="/add">Wyloguj się</NavLink>
       </nav>
     </div>
   );
