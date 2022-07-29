1e6bdd2149546a003ac58b0e2b13b6fff6b8d52c
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-07-29 14:14:56 +0200

    add hyperlink

--- src/views/ErrorView.tsx
+++ src/views/ErrorView.tsx
@@ -8,6 +8,7 @@ export const ErrorView = () => {
       <img src="/img/logo_MegaK.png" alt="logo" />
       <h3>Oops..!</h3>
       <p>Przepraszamy. Strona, której szukasz nie istnieje.</p>
+      <a href="/">Wróć do strony logowania</a>
     </main>
   );
 };
