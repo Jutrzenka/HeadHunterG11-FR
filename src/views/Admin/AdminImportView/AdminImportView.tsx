8bfc091bc70f41ba7ee543161e0bd6ac1f877f4a
Aleksandra Wilk <oleksandra.wilk@gmail.com>
2022-07-29 21:08:04 +0200

    add add file btns

--- src/views/Admin/AdminImportView/AdminImportView.tsx
+++ src/views/Admin/AdminImportView/AdminImportView.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { AiOutlineFileAdd } from 'react-icons/ai';
+
 import './_AdminImportView.scss';
 
 import { AdminLayout } from '../../../components/admin/AdminLayout/AdminLayout';
+import { useToggle } from '../../../utils/hooks/useToggle';
 
 export const AdminImportView = () => {
   return (
     <main className={'view-AdminImportView'}>
       <AdminLayout>
-        <div className="buttons">
-          <h1>przycisk</h1>
-          <h1>przycisk</h1>
+        <div className="import-file-wrap container">
+          <div className="select-file active">
+            <AiOutlineFileAdd />
+            <p>CSV</p>
+          </div>
+          <div className="select-file">
+            <AiOutlineFileAdd />
+            <p>JSON</p>
+          </div>
+          <div className="select-file">
+            <AiOutlineFileAdd />
+            <p>PUT</p>
+          </div>
         </div>
         <div></div>
       </AdminLayout>
