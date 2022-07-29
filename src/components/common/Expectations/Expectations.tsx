3bd3bc8ee29f120476e70bf3934241d844d6dcd8
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-07-29 14:48:25 +0200

    Added Expectations component in common components

---
+++ src/components/common/Expectations/Expectations.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './_Expectations.scss';
+
+interface Props {
+  description: string;
+  value: string;
+}
+
+export const Expectations = ({ description, value }: Props) => {
+  return (
+    <div className={'component-Expectations'}>
+      <div className={'expectations-wrap'}>
+        <div className={'description'}>{description}</div>
+        <div className={'value'}>{value}</div>
+      </div>
+    </div>
+  );
+};
