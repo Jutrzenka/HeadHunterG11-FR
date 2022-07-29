00bd94e4b71f7e86c7ad5ac758cd45f5e520d5a1
Marcel <marcelurban2@tlen.pl>
2022-07-30 00:09:03 +0200

    Add general frame

--- src/views/App/EditCvView/EditCvView.tsx
+++ src/views/App/EditCvView/EditCvView.tsx
@@ -1,11 +1,39 @@
 import React from 'react';
 
 import './_EditCvView.scss';
+import { Header } from '../../../components/common/Header/Header';
 
 export const EditCvView = () => {
   return (
     <main className={'view-EditCvView'}>
+      <Header />
       <h1>EditCvView</h1>
+      <section className="student-contact container"></section>
+
+      <section>
+        <h3>Oceny</h3>
+      </section>
+      <section>
+        <h3>Oczekiwania w stosunku do zatrudnienia</h3>
+      </section>
+      <section>
+        <h3>Edukacja</h3>
+      </section>
+      <section>
+        <h3>Kursy</h3>
+      </section>
+      <section>
+        <h3>Doświadczenie zawodowe</h3>
+      </section>
+      <section>
+        <h3>Portfolio</h3>
+      </section>
+      <section>
+        <h3>Projekt w zespole Scrumowym</h3>
+      </section>
+      <section>
+        <h3>Projekt na zaliczenie</h3>
+      </section>
     </main>
   );
 };
