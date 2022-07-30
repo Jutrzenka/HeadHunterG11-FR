73e4e2ad0405ec72a45d6d1333a80fcd398f1257
Marcel <marcelurban2@tlen.pl>
2022-07-30 02:30:20 +0200

    Refactor structure

--- src/views/App/EditCvView/EditCvView.tsx
+++ src/views/App/EditCvView/EditCvView.tsx
@@ -31,63 +31,176 @@ export const EditCvView = () => {
       {/*  </div>*/}
       {/*</section>*/}
 
-      <div className="student-about">
-        <section>
-          <h3>Adres e-mail</h3>
-        </section>
-        <section>
-          <h3>Numer tel.</h3>
-        </section>
-        <section>
-          <h3>Imię</h3>
-        </section>
-        <section>
-          <h3>Nazwisko</h3>
-        </section>
-        <section>
-          <h3>Login GitHuba</h3>
-        </section>
-        <section>
-          <h3>Krótkie bio</h3>
-        </section>
-      </div>
-
-      <div className="hireExpectations">
-        <section>
-          <h3>Preferowane miejsce pracy</h3>
-        </section>
-        <section>
-          <h3>Docelowe miasto, gdzie chce pracować kandydat</h3>
-        </section>
-        <section>
-          <h3>Oczekiwany typ kontraktu</h3>
-        </section>
-        <section>
-          <h3>Oczekiwane wynagrodzenie miesięczne netto</h3>
-        </section>
-        <section>
-          <h3>Zgoda na odbycie bezpłatnych praktyk/stażu na początek</h3>
-        </section>
-        <section>
-          <h3>Komercyjne doświadczenie w programowaniu</h3>
-        </section>
-      </div>
-
-      <section>
-        <h3>Edukacja</h3>
-      </section>
-      <section>
-        <h3>Kursy</h3>
-      </section>
-      <section>
-        <h3>Doświadczenie zawodowe</h3>
+      <section className="student-about container">
+        <div className="title-wrap">
+          <h3>Ogólne</h3>
+        </div>
+        <div className="content-wrap">
+          <div className="settingsList">
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">Adres e-mail</h3>
+              <span className="settingsListItemContent">
+                marcelurban2@tlen.pl
+              </span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">Numer tel.</h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">Imię</h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">Nazwisko</h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">Login GitHuba</h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">Krótkie bio</h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+          </div>
+        </div>
       </section>
 
-      <section>
-        <h3>Portfolio</h3>
+      <section className="student-hireExpectations container">
+        <div className="title-wrap">
+          <h3>Oczekiwania w stosunku do zatrudnienia</h3>
+        </div>
+        <div className="content-wrap">
+          <div className="settingsList">
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">
+                Preferowane miejsce pracy
+              </h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">
+                Docelowe miasto, gdzie chce pracować kandydat
+              </h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">
+                Oczekiwany typ kontraktu
+              </h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">
+                Oczekiwane wynagrodzenie miesięczne netto
+              </h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">
+                Zgoda na odbycie bezpłatnych praktyk/stażu na początek
+              </h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel">
+                Komercyjne doświadczenie w programowaniu
+              </h3>
+              <span className="settingsListItemContent"></span>
+            </div>
+          </div>
+        </div>
       </section>
-      <section>
-        <h3>Projekt na zaliczenie</h3>
+
+      <section className="student-education container">
+        <div className="title-wrap">
+          <h3>Edukacja</h3>
+        </div>
+        <div className="content-wrap">
+          <div className="settingsList">
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel"></h3>
+              <span className="settingsListItemContent">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aperiam at, est expedita inventore quidem recusandae tenetur
+                voluptate. Iure quibusdam, voluptas!
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="student-courses container">
+        <div className="title-wrap">
+          <h3>Kursy</h3>
+        </div>
+        <div className="content-wrap">
+          <div className="settingsList">
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel"></h3>
+              <span className="settingsListItemContent">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aperiam at, est expedita inventore quidem recusandae tenetur
+                voluptate. Iure quibusdam, voluptas!
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="student-work_experience container">
+        <div className="title-wrap">
+          <h3>Doświadczenie zawodowe</h3>
+        </div>
+        <div className="content-wrap">
+          <div className="settingsList">
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel"></h3>
+              <span className="settingsListItemContent">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aperiam at, est expedita inventore quidem recusandae tenetur
+                voluptate. Iure quibusdam, voluptas!
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="student-portfolio container">
+        <div className="title-wrap">
+          <h3>Portfolio</h3>
+        </div>
+        <div className="content-wrap">
+          <div className="settingsList">
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel"></h3>
+              <span className="settingsListItemContent">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aperiam at, est expedita inventore quidem recusandae tenetur
+                voluptate. Iure quibusdam, voluptas!
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="student-project container">
+        <div className="title-wrap">
+          <h3>Projekt na zaliczenie</h3>
+        </div>
+        <div className="content-wrap">
+          <div className="settingsList">
+            <div className="settingsListItem">
+              <h3 className="settingsListItemLabel"></h3>
+              <span className="settingsListItemContent">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Aperiam at, est expedita inventore quidem recusandae tenetur
+                voluptate. Iure quibusdam, voluptas!
+              </span>
+            </div>
+          </div>
+        </div>
       </section>
     </main>
   );
