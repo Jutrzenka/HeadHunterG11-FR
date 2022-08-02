a7346298b7d00dce9c2dbe5286bccc776b3f0974
Marcel <marcelurban2@tlen.pl>
2022-08-02 20:15:36 +0200

    Added trim "\n" on focusout

--- src/components/common/Textarea/Textarea.tsx
+++ src/components/common/Textarea/Textarea.tsx
@@ -17,53 +17,40 @@ export const Textarea = ({
   minLength,
 }: Props) => {
   const [inputValue, setInputValue] = useState(value);
-  const [textAreaHeight, setTextAreaHeight] = useState({
-    height: 'auto',
-  });
   const activeSlideRef = useRef<HTMLTextAreaElement>(null);
 
   useEffect(() => {
     if (activeSlideRef.current != null) {
       const { scrollHeight } = activeSlideRef.current;
-
-      setTextAreaHeight({
-        height: `${scrollHeight}px`,
-      });
+      activeSlideRef.current.style.height = `${scrollHeight}px`;
     }
   }, []);
+
+  useEffect(() => {
+    if (activeSlideRef.current != null) {
+      const { scrollHeight, style } = activeSlideRef.current;
+      style.height = `${scrollHeight}px`;
+    }
+  }, [inputValue]);
 
   return (
     <div className={'component-Textarea'}>
       <label>
         {title && <p>{title}</p>}
+        <br />
         <textarea
           ref={activeSlideRef}
-          style={textAreaHeight}
           value={inputValue}
           onChange={(event) => {
-            // setTextAreaHeight((prev) => ({
-            //   ...prev,
-            //   height: `40px`,
-            // }));
-            // event.target.style.height = '40px';
-
-            // setTextAreaHeight(
-            //   {
-            //     height: `40px`,
-            // },
-            // () => {
-            //   setTextAreaHeight({
-            //     height: `${event.target.scrollHeight}px`,
-            //   });
-            // },
-            // );
-
-            setTextAreaHeight({
-              height: `${event.target.scrollHeight}px`,
-            });
-
-            setInputValue(event.target.value.trimStart());
-            // value(event.target.value.trim());
+            event.target.style.height = '40px';
+            // event.target.style.height = `${event.target.scrollHeight}px`;
+            setInputValue(event.target.value);
+          }}
+          onBlur={(event) => {
+            if (event.target.value.trim() !== inputValue) {
+              event.target.style.height = '40px';
+            }
+            setInputValue(event.target.value.trim());
           }}
         ></textarea>
       </label>
